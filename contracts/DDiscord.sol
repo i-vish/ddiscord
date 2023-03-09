c14667505bd8347b5ae19d098c796c8fe2b00477
Indresh Vishwakarma <i.vish1337@gmail.com>
2023-03-09 22:59:23 +0530

    chore mint and withdraw added

--- contracts/DDiscord.sol
+++ contracts/DDiscord.sol
@@ -2,14 +2,66 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DDiscord is ERC721 {
     address public owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter public totalChannels;
+    Counters.Counter public totalSupply;
+
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function createChannel(
+        string memory _name,
+        uint256 _cost
+    ) public onlyOwner {
+        totalChannels.increment();
+        uint256 _channelID = totalChannels.current();
+        channels[_channelID] = Channel(_channelID, _name, _cost);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+        return channels[_id];
+    }
+
+    function mint(uint256 _id) public payable {
+        // Join Channel
+        //Mint NFT
+        require(_id != 0);
+        require(_id <= totalChannels.current());
+        require(hasJoined[_id][msg.sender] == false);
+        require(msg.value >= channels[_id].cost);
+
+        hasJoined[_id][msg.sender] = true;
+        totalSupply.increment();
+        uint256 _currentSupply = totalSupply.current();
+
+        _safeMint(msg.sender, _currentSupply);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
--- test/contracts/ddiscord.js
+++ test/contracts/ddiscord.js
@@ -1,17 +1,30 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const tokens = (n) => ethers.utils.parseUtils(n.toString(), "ether");
+const tokens = (n) => ethers.utils.parseUnits(n.toString(), "ether");
 
 describe("DDiscord", function () {
   let ddiscord, deployer, user;
   const NAME = "DDiscord";
   const SYMBOL = "DDIS";
+  const CHANNEL_NAME = "general";
+
   beforeEach(async () => {
+    // Setup Accounts
     [deployer, user] = await ethers.getSigners();
+
+    // Deploy Contract
     const Dapp = await ethers.getContractFactory("DDiscord");
     ddiscord = await Dapp.deploy(NAME, SYMBOL);
+
+    // Create Channel
+    const transaction = await ddiscord
+      .connect(deployer)
+      .createChannel(CHANNEL_NAME, tokens(1));
+
+    await transaction.wait();
   });
+
   describe("Deployment", function () {
     it("Sets the Name", async () => {
       let result = await ddiscord.name();
@@ -26,6 +39,75 @@ describe("DDiscord", function () {
     it("Sets the Owner", async () => {
       let result = await ddiscord.owner();
       expect(result).to.equal(deployer.address);
+    });
+  });
+
+  describe("Creating Channels", () => {
+    it("Return Total Channels", async () => {
+      const result = await ddiscord.totalChannels();
+      expect(result).to.be.equal(1);
+    });
+
+    it("Return channel Attributes", async () => {
+      const channel = await ddiscord.getChannel(1);
+      expect(channel.id).to.be.equal(1);
+      expect(channel.name).to.be.equal(CHANNEL_NAME);
+      expect(channel.cost).to.be.equal(tokens(1));
+    });
+  });
+
+  describe("Joining Channels", () => {
+    const ID = 1;
+    const AMOUNT = tokens(1);
+
+    beforeEach(async () => {
+      const transaction = await ddiscord
+        .connect(user)
+        .mint(ID, { value: AMOUNT });
+      await transaction.wait();
+    });
+
+    it("Joins the user", async () => {
+      const result = await ddiscord.hasJoined(ID, user.address);
+      expect(result).to.be.equal(true);
+    });
+
+    it("Increases total supply", async () => {
+      const result = await ddiscord.totalSupply();
+      expect(result).to.be.equal(ID);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(ddiscord.address);
+      expect(result).to.be.equal(AMOUNT);
+    });
+  });
+
+  describe("Withdrawing", () => {
+    const ID = 1;
+    const AMOUNT = tokens(10);
+    let balanceBefore;
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      let transaction = await ddiscord
+        .connect(user)
+        .mint(ID, { value: AMOUNT });
+      await transaction.wait();
+
+      transaction = await ddiscord.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates the owner balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(ddiscord.address);
+      expect(result).to.equal(0);
     });
   });
 });
